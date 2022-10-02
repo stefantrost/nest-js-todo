f84589aa83b4d8801d83ce677f6d204b3a58bc5b
Stefan Trost <stefan@techscene.de>
2022-10-02 13:54:41 +0200

    Add todo crud stumps and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todos')
+    .setDescription('The todo API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('explorer', app, document);
+
   await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
